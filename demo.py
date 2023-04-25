3845090756b26f7c42582e0c6cbb2e00fe273b07
PhucHuy3011 <phuchuy30111999@gmail.com>
2023-04-25 19:18:24 +0700

    first commit

---
+++ demo.py
@@ -0,0 +1,32 @@
+# ham print dung de in 
+# cls dùng đển clean màn hình terminal
+print('Hello World')
+a =1
+b=2
+c=1+2
+print(c)
+for chu in 'huydeptrai' :
+    print('Chữ Cái đầu tiên',chu)
+A = [1,2,3,4,5,6,7,8,9]
+tong = 0
+for a in A :
+    tong +=a
+
+print("Tong cac so nguyen la: ",tong)
+
+age:int = 23
+print(age)
+name:str = 'Nguyễn Phan Phúc Huy'
+email:str = "phuchuy30111999@gmail.com"
+print(email)
+print(name)
+for inchu in name:
+    print('Phân tách chữ trong chuỗi name',inchu)
+
+sole:float = 7.5
+sochan:int = 8
+print('Tổng hai số chẳn và lẻ :',sole+sochan)
+
+#khai báo biến boolean chứa duy nhất hai giá trị true và false
+status1:bool = True
+print(status1)
